c94da3fc76eb89131736c5eb7efd48864c8725b7
mvalbuquerque <albuquerquenet@gmail.com>
2023-08-28 19:45:57 -0300

    desafio perfil usuario

--- 06_Quarta-Feira_23_08_2023/06_DesafioPerfildoUsuario/DesafioPerfilUsuarioSolucao.kt
+++ 06_Quarta-Feira_23_08_2023/06_DesafioPerfildoUsuario/DesafioPerfilUsuarioSolucao.kt
@@ -1,13 +1,38 @@
-fun main() {
-    // Declaração e atribuição de variáveis
-    val nome = "João"
-    var idade = 30
-    val email = "joao@example.com"
-    var usuarioAtivo = true
+//Desafio Perfil Usuario  - Com Input
 
-    // Exibição das informações no console
+fun main() {
+
+    //Solicitando entrada dos dados do usuario
+    println("Digite seu nome:")
+    val nome = readLine()!!
+
+    println("Digite seu idade:")
+    val idade = readLine()!!.toInt()
+
+    println("Digite seu email:")
+    val email = readLine()!!
+
+    println("O usuário está ativo?(true/false):")
+    val usuarioAtivo = readLine()!!.toBoolean()
+
+    //Exibição das informações no console 
     println("Nome: $nome")
     println("Idade: $idade")
     println("Email: $email")
-    println("Usuário Ativo: $usuarioAtivo")
+    print("Ativo: $usuarioAtivo")
 }
+
+
+// fun main() {
+//     // Declaração e atribuição de variáveis
+//     val nome = "João"
+//     var idade = 30
+//     val email = "joao@example.com"
+//     var usuarioAtivo = true
+
+//     // Exibição das informações no console
+//     println("Nome: $nome")
+//     println("Idade: $idade")
+//     println("Email: $email")
+//     println("Usuário Ativo: $usuarioAtivo")
+// }
