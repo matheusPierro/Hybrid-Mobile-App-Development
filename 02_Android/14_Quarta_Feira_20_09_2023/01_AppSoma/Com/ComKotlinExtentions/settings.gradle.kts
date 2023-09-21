fd4967491e9499ce21d8294ee70d2709d82364e9
mvalbuquerque <albuquerquenet@gmail.com>
2023-09-20 22:33:01 -0300

    update

---
+++ 02_Android/14_Quarta_Feira_20_09_2023/01_AppSoma/Com/ComKotlinExtentions/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Com_Kotlin_Extation"
+include(":app")
+ 
